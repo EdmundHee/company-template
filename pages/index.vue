3498862f2284b7f04cab10fd9f7176a5f9f94d5d
Edmund Hee <edmund.hee05@gmail.com>
2020-07-29 21:38:37 +0800

    Workshop for Company Template

--- pages/index.vue
+++ pages/index.vue
@@ -11,24 +11,22 @@
           :second_header ( Header 2 Text)
           :descriptions ( Descriptions )
         -->
-        <HeaderContent 
-          imgURL="/img/slider1.jpeg"
-          header_txt="Company Template by LEAD"
-          second_header="Second Header"
-          descriptions="Header Descriptions"
+        <HeaderContent
+          imgURL="https://assets.bonappetit.com/photos/5c366551f212512d0e6cefd0/16:9/w_2560%2Cc_limit/Basically-Coffee-0219-03.jpg"
+          header_txt="The LEAD"
+          second_header="Institute of Growth"
+          descriptions="FullStack Web Development Workshop"
         ></HeaderContent>
         <!-- 
           About Content 
 
           PARAMETERS
           :imgURL ( URL for about us )
-         -->
-        <AboutContent
-          imgURL="/img/about.jpg"
-        >
-          <template v-slot:about>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis dolorum rerum hic exercitationem odit, soluta eaque eos corporis quam alias ipsum repudiandae blanditiis tempore eligendi numquam temporibus! Molestias, quos beatae?
-          </template>
+        -->
+        <AboutContent imgURL="https://www.cornellsci.com/images/about-002.png">
+          <template
+            v-slot:about
+          >LEAD, is the institute that focus on growth. We help those that want to change their career or upskill.</template>
         </AboutContent>
 
         <ServiceContent>
@@ -40,48 +38,34 @@
               PARAMETERS
 
               :imgURL (image url)
-             -->
-            <Service
-              imgURL="/img/icon1.png"
-              title="Service Title"
-            >
-              <template v-slot:content>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus excepturi quisquam temporibus non nisi doloribus cumque cum molestiae odio velit ratione perspiciatis assumenda numquam quam porro, sed quasi quia possimus?
-              </template>
+            -->
+            <Service imgURL="/img/icon1.png" title="Turn idea into Reality">
+              <template v-slot:content>Help and guide to turn ideas into Applications</template>
             </Service>
 
-            <Service
-              imgURL="/img/icon2.png"
-              title="Service Title"
-            >
-              <template v-slot:content>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus excepturi quisquam temporibus non nisi doloribus cumque cum molestiae odio velit ratione perspiciatis assumenda numquam quam porro, sed quasi quia possimus?
-              </template>
+            <Service imgURL="/img/icon2.png" title="Service Title">
+              <template
+                v-slot:content
+              >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus excepturi quisquam temporibus non nisi doloribus cumque cum molestiae odio velit ratione perspiciatis assumenda numquam quam porro, sed quasi quia possimus?</template>
             </Service>
 
-            <Service
-              imgURL="/img/icon3.png"
-              title="Service Title"
-            >
-              <template v-slot:content>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus excepturi quisquam temporibus non nisi doloribus cumque cum molestiae odio velit ratione perspiciatis assumenda numquam quam porro, sed quasi quia possimus?
-              </template>
+            <Service imgURL="/img/icon3.png" title="Service Title">
+              <template
+                v-slot:content
+              >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus excepturi quisquam temporibus non nisi doloribus cumque cum molestiae odio velit ratione perspiciatis assumenda numquam quam porro, sed quasi quia possimus?</template>
             </Service>
 
-            <Service
-              imgURL="/img/icon4.png"
-              title="Service Title"
-            >
-              <template v-slot:content>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus excepturi quisquam temporibus non nisi doloribus cumque cum molestiae odio velit ratione perspiciatis assumenda numquam quam porro, sed quasi quia possimus?
-              </template>
+            <Service imgURL="/img/icon4.png" title="Service Title">
+              <template
+                v-slot:content
+              >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus excepturi quisquam temporibus non nisi doloribus cumque cum molestiae odio velit ratione perspiciatis assumenda numquam quam porro, sed quasi quia possimus?</template>
             </Service>
           </template>
         </ServiceContent>
 
         <!-- 
           Portfolio
-         -->
+        -->
         <PortfolioContent>
           <template v-slot:portfolio>
             <!-- 
@@ -92,14 +76,31 @@
               :name ( Name of the Project )
               :project ( Name of Project Type)
               :projectURL ( Link Address to project)
-             -->
+            -->
             <Project
-                imgURL='/img/about.jpg'
-                name='Project Name'
-                project='Project Type'
-                projectURL='https://google.com'
-              />
-
+              imgURL="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
+              name="Company Template"
+              project="NuxtJS App"
+              projectURL="https://github.com/EdmundHee/company-template"
+            />
+            <Project
+              imgURL="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
+              name="Company Template"
+              project="NuxtJS App"
+              projectURL="https://github.com/EdmundHee/company-template"
+            />
+            <Project
+              imgURL="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
+              name="Company Template"
+              project="NuxtJS App"
+              projectURL="https://github.com/EdmundHee/company-template"
+            />
+            <Project
+              imgURL="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
+              name="Company Template"
+              project="NuxtJS App"
+              projectURL="https://github.com/EdmundHee/company-template"
+            />
           </template>
         </PortfolioContent>
         <ContactContent
@@ -114,5 +115,10 @@
 </template>
 <script>
 export default {
-}
+  head() {
+    return {
+      title: "The LEAD"
+    };
+  }
+};
 </script>
